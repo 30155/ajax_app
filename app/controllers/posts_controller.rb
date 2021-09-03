1e2bae2c294ac1e917ba430c0826bb593b17fb74
30155 <awlc.v41st@gmail.com>
2021-09-03 12:41:29 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,12 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content]) #新たに投稿されたメモの内容を変数postに格納
+    # redirect_to action: :index 以下に変更
+    render json:{ post: post }  #json:の部分をjsonオプションといい、これを指定することによって、直後に記述した{ post: post }というデータをJSON形式で返却することができます。
+    #上で定義した変数postの値を、postというキーとセットでJavaScriptに送信しています。
   end
 end
+
+
+# renderメソッドとは url：https://pikawaka.com/rails/render ここ詳しい
